5b857053bf9bc22c6cd11d9361fd64c2ae4f64bb
VijayManjunath <vijay.manjunath@position2.com>
2023-08-15 22:04:32 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,24 +2,12 @@ import express from 'express';
 import cors from 'cors';
 
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: true }));
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Upload files to the 'uploads' directory
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage });
 
 // Serve static files
 app.use(express.static('public'));
